9f9651c3538cd43ca8fed9abed7374f5f7a3a63a
LDY1998 <caliudeyu2017@gmail.com>
2021-04-07 20:58:40 -0700

    add tests and succefully compile with stub API

---
+++ src/kvs.rs
@@ -0,0 +1,19 @@
+
+pub struct KvStore{
+
+}
+
+
+impl KvStore {
+    pub fn new() {
+        unimplemented!("");
+    }
+
+    pub fn get(&self, key: String) {
+        unimplemented!("not implemented!");
+    }
+
+    pub fn set(&self, key: String, value: String) {
+        unimplemented!("not implemented!");
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+
+pub use kvs::KvStore;
+pub mod kvs;
