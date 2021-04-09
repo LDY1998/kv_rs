805e030069588e867635365f1aa066dd52d334c8
LDY1998 <caliudeyu2017@gmail.com>
2021-04-08 21:51:43 -0700

    refactor clap to structopts

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,55 @@
 extern crate clap;
+
+#[macro_use]
+extern crate structopt;
+
 use clap::{Arg, App, SubCommand, AppSettings};
 use std::process::exit;
+use structopt::StructOpt;
 
 fn main() {
 
-    let matches = App::new(env!("CARGO_PKG_NAME"))
-            .version(env!("CARGO_PKG_VERSION"))
-            .author(env!("CARGO_PKG_VERSION"))
-            .about(env!("CARGO_PKG_DESCRIPTION"))
-            .setting(AppSettings::DisableHelpSubcommand)
-            .setting(AppSettings::SubcommandRequiredElseHelp)
-            .setting(AppSettings::VersionlessSubcommands)
-            .subcommand(
-                SubCommand::with_name("get")
-                    .about("Get the value of a given key string")
-                    .arg(Arg::with_name("Key").help("A string key").required(true))
-            )
-            .subcommand(
-                SubCommand::with_name("set")
-                    .about("Set the value of given key")
-                    .arg(Arg::with_name("Key").help("A string key").required(true))
-                    .arg(Arg::with_name("Value").help("A string value").required(true))
-            )
-            .subcommand(
-                SubCommand::with_name("rm")
-                    .about("Remove the value with given key")
-                    .arg(Arg::with_name("Key").help("A string key").required(true))
-            )
-            .get_matches();
+    match KvCli::from_args() {
+        KvCli::Get {key} => {
+            eprintln!("unimplemented");
+            exit(1);
+        },
+        KvCli::Set {key, value} => {
+            eprintln!("unimplemented");
+            exit(1);
+        },
+        KvCli::Remove {key} => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+    }
 
-            match matches.subcommand() {
-                ("get", Some(key)) => {
-                   eprintln!("unimplemented");
-                   exit(1); 
-                },
-                ("set", Some(kv)) => {
-                   eprintln!("unimplemented");
-                   exit(1); 
-                },
-                ("rm", Some(key)) => {
-                    eprintln!("unimplemented");
-                    exit(1);
-                },
-                (_) => {
-                    eprintln!("unimplemented");
-                    exit(1);
-                },
-            }
+}
+
+// #[derive(StructOpt, Debug)]
+// #[structopt(name = env!("CARGO_PKG_NAME"), about = env!("CARGO_PKG_DESCRIPTION"))]
+// struct Opt {
+//     #[structopt(subcommand)]
+//     subcommand: KvCli
+// }
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = env!("CARGO_PKG_NAME"), about = env!("CARGO_PKG_DESCRIPTION"))]
+enum KvCli { 
+    #[structopt(name = "get")]
+    Get {
+        key: String,
+    },
+
+    #[structopt(name="set")]
+    Set {
+        key: String,
+        value: String,
+    },
+
+    #[structopt(name="rm")]
+    Remove {
+        key: String
+    }
 
 }
